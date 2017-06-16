a389c7aa0b34ad05de3846646534501b8b4c0d50
Ravi Kumar G <ravikumarygp.mahesh@gmail.com>
2017-06-17 02:49:43 +0530

    Create skeleton webpaco project for angular templates

---
+++ index.js
@@ -0,0 +1,7 @@
+window.jQuery = window.$ =  require('./js/jquery-3.2.1.min.js');
+require("./css/bootstrap.css")
+require("./css/styles.css")
+require("./js/bootstrap.js")
+require('./js/angular.js')
+require('./js/app.js')
+require('./index.html')
---
+++ js/app.js
@@ -0,0 +1,5 @@
+var app=angular.module("app",[])
+
+app.controller("testctrl",function($scope){
+    $scope.helloworld="Hello World!";
+})
